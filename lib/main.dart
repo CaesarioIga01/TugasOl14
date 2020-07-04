1490a3c6c6c49e0a3cae1e48e1d82cea71f95591
CaesarioIga01 <caesarioiga08@gmail.com>
2020-07-04 17:26:55 +0700

    commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ol14/main_page.dart';
+import 'package:ol14/model/monster.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
+import 'package:hive/hive.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var appDocumentDirectory =
+      await pathProvider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  Hive.registerAdapter(MonsterAdapter(), 0);
+  runApp(TugasOL14());
+}
+
+class TugasOL14 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
